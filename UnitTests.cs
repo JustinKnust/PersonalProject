593f1b26a29c08ecdbf0b751dcd0f368dfd651f0
JustinKnust <59981836+JustinKnust@users.noreply.github.com>
2020-05-03 17:58:25 -0500

    Add files via upload

---
+++ UnitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Food_Ordering;
+
+namespace Food_OrderingTests
+{
+    (TestClass)
+    public class UnitTest1
+    {
+        TestMethod
+        public void HamburgerMethod()
+        {
+            Order order = new Order();
+            order.addordertype("hamburger");
+            order.addordertype("cheese");
+            string details = order.getorderdetails();
+
+            Assert.AreEqual("Cheese Burger", details);
+        }
+        TestMethod
+        public void HotdogMethod()
+        {
+            Order order = new Order();
+
+            order.addordertype("hotdog");
+            order.addorderingdetail("mustard", 0);
+            string toppings = order.getorderdetails();
+
+            Assert.AreEqual("hotdog with mustard", toppings);
+        }
+    }
+}
