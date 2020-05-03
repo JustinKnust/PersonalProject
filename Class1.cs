a25e50b38ce7c5432722e39fa67c0d8bb2bdbec9
JustinKnust <59981836+JustinKnust@users.noreply.github.com>
2020-05-03 17:59:50 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Food_Ordering
+
+{
+    public class Order
+    {
+        string _ordertype = "";
+        string _orderdetail = "";
+        int _numtoppings = 0;
+
+        public Order()
+            {
+
+            }
+        public void addordertype(string ordertype)
+        {
+            _ordertype = ordertype;
+        }
+        public void addorderingdetail(string orderdetail, int numtoppings)
+        {
+            _orderdetail = orderdetail;
+            _numtoppings = numtoppings;
+        }
+        public string getorderdetails()
+        {
+            if (_ordertype == "hamburger")
+            {
+                return _orderdetail + " " + _ordertype;
+            }
+            else if (_ordertype == "hotdog")
+            {
+                return _numtoppings + " " + _orderdetail + " " + _ordertype;
+            }
+            return "unknown Order";
+        }
+    }
+}
